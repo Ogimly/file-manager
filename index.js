e34418cb59a496310577cac7e8f710ae9aa9f714
ogimly <ogimly@gmail.com>
2022-12-05 02:52:26 +0500

    feat: add transform stream for user command handling

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { chdir, cwd, env, exit, stdout, stderr } from 'process';
 import { EOL, homedir } from 'os';
+import { Transform, pipeline } from 'stream';
+
 import { getUsername } from './src/utils.js';
 import { errorCode } from './src/const.js';
-import { write } from 'fs';
 
 const start = () => {
   try {
     const username = getUsername();
-
     if (!username) {
       throw new Error(errorCode.noUser);
     }
@@ -15,20 +15,42 @@ const start = () => {
 
     chdir(homedir());
 
-    stdout.write(EOL + `Welcome to the File Manager, ${env.username}!` + EOL);
-    stdout.write(`You are currently in ${cwd()} :`);
+    stdout.write(EOL + `Welcome to the File Manager, ${env.username}!${EOL}`);
+    stdout.write(`You are currently in ${cwd()}${EOL}`);
 
-    stdout.write(
-      EOL + `Thank you for using File Manager, ${env.username}, goodbye!` + EOL
-    );
+    const mainProcess = new Transform({
+      transform(chunk, encoding, callback) {
+        const command = chunk.toString().replace(EOL, '');
+
+        stdout.write(`command is ${command}${EOL}`);
+
+        switch (command) {
+          case '.exit':
+            stdout.write(
+              `Thank you for using File Manager, ${env.username}, goodbye!${EOL}`
+            );
+            process.exit(0);
+        }
+
+        stdout.write(`You are currently in ${cwd()}${EOL}`);
+
+        callback();
+      },
+    });
+
+    pipeline(process.stdin, mainProcess, process.stdout, (error) => {
+      if (error) {
+        stderr.write(`Operation failed: ${error}${EOL}`);
+      }
+    });
   } catch (error) {
     switch (error.message) {
       case errorCode.noUser:
-        stderr.write('Please enter your username!' + EOL);
+        stderr.write(`Please enter your username!${EOL}`);
         break;
 
       default:
-        stderr.write(error.message);
+        stderr.write(`${error}${EOL}`);
         break;
     }
     exit();
