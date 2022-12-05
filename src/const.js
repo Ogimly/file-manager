d546ae66c1679bce13f219d6b8f95d890472c491
ogimly <ogimly@gmail.com>
2022-12-06 01:39:44 +0500

    feat: add os commands handling

--- src/const.js
+++ src/const.js
@@ -2,12 +2,18 @@ import { stdin as input, stdout as output, stderr as errOutput } from 'process';
 export { input, output, errOutput };
 
 import { up, cd, ls } from './navigation.js';
+import { eol, cpus, homedir, username, architecture } from './operating-system.js';
 
 export const FileManagerHandlers = [
   { command: '.EXIT', handler: process.exit },
   { command: 'UP', handler: up },
   { command: 'CD', handler: cd },
   { command: 'LS', handler: ls },
+  { command: '--EOL', handler: eol },
+  { command: '--CPUS', handler: cpus },
+  { command: '--HOMEDIR', handler: homedir },
+  { command: '--USERNAME', handler: username },
+  { command: '--ARCHITECTURE', handler: architecture },
 ];
 
 export const errorCode = {
---
+++ src/operating-system.js
@@ -0,0 +1,47 @@
+import { EOL, cpus as osCPUs, homedir as osHomedir, userInfo, arch } from 'os';
+
+import { output } from './const.js';
+import { writeFailedMessage } from './utils.js';
+
+export const eol = () => {
+  try {
+    output.write(`EOL (default system End-Of-Line): ${JSON.stringify(EOL)}`);
+  } catch (error) {
+    writeFailedMessage(error);
+  }
+};
+
+export const cpus = () => {
+  try {
+    const CPUArray = osCPUs().map(({ model, speed }) => ({
+      Model: model,
+      Speed: `${speed / 1000}GHz`,
+    }));
+    output.write(`Overall amount of CPUs: ${CPUArray.length}${EOL}`);
+    console.table(CPUArray);
+  } catch (error) {
+    writeFailedMessage(error);
+  }
+};
+
+export const homedir = () => {
+  try {
+    output.write(`Home directory: ${osHomedir()}`);
+  } catch (error) {
+    writeFailedMessage(error);
+  }
+};
+export const username = () => {
+  try {
+    output.write(`Current system user name: ${userInfo().username}`);
+  } catch (error) {
+    writeFailedMessage(error);
+  }
+};
+export const architecture = () => {
+  try {
+    output.write(`CPU architecture: ${arch()}`);
+  } catch (error) {
+    writeFailedMessage(error);
+  }
+};
