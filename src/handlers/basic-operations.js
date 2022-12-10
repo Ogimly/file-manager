144518f4ed5dea68617ca5c69c636ef691e5df6e
ogimly <ogimly@gmail.com>
2022-12-10 14:26:19 +0500

    feat: add path checks to basic operation handler

--- src/handlers/basic-operations.js
+++ src/handlers/basic-operations.js
@@ -1,14 +1,21 @@
 import { createReadStream, createWriteStream } from 'fs';
-import { rename, rm as fsRm } from 'fs/promises';
+import { rename, rm as remove } from 'fs/promises';
 import { finished, pipeline } from 'stream';
-import { join, basename } from 'path';
+import { join } from 'path';
 
 import { errorCode } from '../const.js';
 import { writeMessage } from '../utils/input-output.js';
+import { checkAsDirectory, checkAsFile, getFileName } from '../utils/files.js';
 
-export const cat = (pathToFile) =>
-  new Promise((resolve, reject) => {
-    if (!pathToFile) reject(new Error(errorCode.noUrl));
+export const cat = (path) =>
+  new Promise(async (resolve, reject) => {
+    const result = await checkAsFile(path);
+    if (result.error) {
+      reject(new Error(result.error));
+      return;
+    }
+
+    const { pathToFile } = result;
 
     const readStream = createReadStream(pathToFile, 'utf-8');
     readStream.on('data', (chunk) => {
@@ -43,64 +50,81 @@ export const add = (fileName) =>
     writeStream.close();
   });
 
-export const rn = async (pathToFile, fileName) => {
-  if (!pathToFile) throw new Error(errorCode.noUrl);
+export const rn = async (path, fileName) => {
+  const result = await checkAsFile(path);
+  if (result.error) {
+    throw new Error(result.error);
+  }
+
+  const { pathToFile } = result;
+
   if (!fileName) throw new Error(errorCode.noFileName);
 
-  const oldName = basename(pathToFile);
+  const oldName = getFileName(pathToFile);
 
   await rename(pathToFile, pathToFile.replace(oldName, fileName));
 
   writeMessage(`"${oldName}" renamed to "${fileName}"`);
 };
 
-export const cp = async (pathToFile, pathDest) =>
-  new Promise((resolve, reject) => {
-    if (!pathToFile || !pathDest) {
-      reject(new Error(errorCode.noUrl));
-    } else {
-      const fileName = basename(pathToFile);
-      const newPathToFile = join(pathDest, fileName);
-
-      const readStream = createReadStream(pathToFile);
-      const writeStream = createWriteStream(newPathToFile);
-
-      pipeline(readStream, writeStream, (error) => {
-        if (error) {
-          reject(error);
-        }
-      });
-
-      finished(readStream, (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          writeMessage(`"${fileName}" copied to "${pathDest}"`);
-          resolve();
-        }
-      });
-
-      finished(writeStream, (error) => {
-        if (error) {
-          reject(error);
-        }
-      });
+export const cp = async (path, pathDest) =>
+  new Promise(async (resolve, reject) => {
+    let result = await checkAsFile(path);
+    if (result.error) {
+      reject(new Error(errorCode.notFileSrc));
+      return;
     }
+    const { pathToFile } = result;
+
+    result = await checkAsDirectory(pathDest);
+    if (result.error) {
+      reject(new Error(errorCode.notDirectoryDest));
+      return;
+    }
+    const { pathToDirectory } = result;
+
+    const fileName = getFileName(pathToFile);
+    const newPathToFile = join(pathToDirectory, fileName);
+
+    const readStream = createReadStream(pathToFile);
+    const writeStream = createWriteStream(newPathToFile);
+
+    pipeline(readStream, writeStream, (error) => {
+      if (error) {
+        reject(error);
+      }
+    });
+
+    finished(readStream, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        writeMessage(`"${fileName}" copied to "${pathDest}"`);
+        resolve();
+      }
+    });
+
+    finished(writeStream, (error) => {
+      if (error) {
+        reject(error);
+      }
+    });
   });
 
-export const rm = async (pathToFile) => {
-  if (!pathToFile) throw new Error(errorCode.noUrl);
+export const rm = async (path) => {
+  const result = await checkAsFile(path);
+  if (result.error) throw new Error(result.error);
 
-  const fileName = basename(pathToFile);
+  const { pathToFile } = result;
 
-  await fsRm(pathToFile);
+  const fileName = getFileName(pathToFile);
+
+  await remove(pathToFile);
 
   writeMessage(`"${fileName}" removed!`);
 };
 
 export const rv = async (pathToFile, pathDest) => {
-  if (!pathToFile || !pathDest) throw new Error(errorCode.noUrl);
-
   try {
     await cp(pathToFile, pathDest);
     await rm(pathToFile);
